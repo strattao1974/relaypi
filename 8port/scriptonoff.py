3ae86cce09739e171e12f4cd8344081c03bfe6d2
strattao1974 <admin@stratfordmusic.com.au>
2017-02-05 09:19:50 +1100

    Add files via upload

---
+++ 8port/scriptonoff.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# init list with pin numbers
+
+pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+
+# loop through pins and set mode and state to 'low'
+
+for i in pinList: 
+    GPIO.setup(i, GPIO.OUT) 
+    GPIO.output(i, GPIO.HIGH)
+
+# time to sleep between operations in the main loop
+
+SleepTimeL = 0.1
+SleepTimeS = 10
+
+try:
+    
+   while True:
+    
+        GPIO.output(2,GPIO.LOW);
+	time.sleep(SleepTimeL)
+	  
+  	GPIO.output(3,GPIO.LOW);
+  	time.sleep(SleepTimeL)
+  
+	GPIO.output(4,GPIO.LOW);
+  	time.sleep(SleepTimeL)
+  	GPIO.output(2,GPIO.HIGH)
+  
+  	GPIO.output(17,GPIO.LOW);
+  	time.sleep(SleepTimeL)
+  	GPIO.output(3,GPIO.HIGH)
+  
+  	GPIO.output(27,GPIO.LOW);
+  	time.sleep(SleepTimeL)
+  	GPIO.output(4,GPIO.HIGH)
+  
+  	GPIO.output(22,GPIO.LOW);
+  	time.sleep(SleepTimeL)
+  	GPIO.output(17,GPIO.HIGH)
+  
+  	GPIO.output(10,GPIO.LOW);
+  	time.sleep(SleepTimeL)  
+  	GPIO.output(27,GPIO.HIGH)
+  
+  	GPIO.output(9,GPIO.LOW);  
+  	time.sleep(SleepTimeL)
+  
+  	GPIO.output(22,GPIO.HIGH)
+  	time.sleep(SleepTimeL)
+  
+  	GPIO.output(10,GPIO.HIGH)
+  	time.sleep(SleepTimeL)
+   
+  	GPIO.output(9,GPIO.HIGH)
+  	time.sleep(SleepTimeL)
+  
+  	GPIO.cleanup()
+  	time.sleep(SleepTimeL)
+	
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+  print "  Quit"
+
+  # Reset GPIO settings
+
+  GPIO.cleanup()
+
+# find more information on this script at
+# http://youtu.be/oaf_zQcrg7g
